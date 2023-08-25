f65048c80c1a92b5ae27ff69986a4b6757b3ebfb
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-08-25 19:37:25 +0200

    k

---
+++ prueba.cpp
@@ -0,0 +1,19 @@
+#include "pila_enlazada.hpp"
+#include "iostream"
+using namespace std;
+
+int main(){
+    Pila<int> pilita;
+    int cambio = 90;
+
+    pilita.push(10);
+    pilita.push(20);
+    pilita.push(40);
+
+    cambio =  pilita.tope() - 30;
+
+    pilita.pop();
+    pilita.push(cambio);
+    cout<<pilita.tope();
+
+}
